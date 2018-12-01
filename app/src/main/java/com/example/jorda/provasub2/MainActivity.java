daad53d04ea2cd60ccf038c33b0b420f36c2ab35
jordanavi <jordanavilela01@gmail.com>
2018-12-01 17:36:44 -0200

    Busca dados

--- app/src/main/java/com/example/jorda/provasub2/MainActivity.java
+++ app/src/main/java/com/example/jorda/provasub2/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.jorda.provasub2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.jorda.provasub2.Model.Currently;
+import com.example.jorda.provasub2.Model.Exemple;
 import com.example.jorda.provasub2.Service.InterfaceDeServicos;
 import com.example.jorda.provasub2.Service.RetrofitService;
 
+import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
@@ -19,28 +22,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       muitaTreta();
+       BuscaDados();
     }
 
-    private void muitaTreta() {
-
-        /*Esta classe imprime a imagem a partir de parte do jason recebido pela busca retrofit */
-
+    private void BuscaDados() {
+        /*Esta classe busca o os valores a partir do jason recebido em busca retrofit */
         InterfaceDeServicos service = RetrofitService.getServico();
-        retrofit2.Call<Currently> call = service.consulta();
 
-        call.enqueue(new Callback<Currently>() {
+        EditText lat = (EditText)findViewById(R.id.latitude);
+        EditText longi = (EditText)findViewById(R.id.longitude);
+
+        double latitudes = Double.parseDouble(lat.getText().toString());
+        double longitudes = Double.parseDouble(longi.getText().toString());
+
+        Call<Exemple> call = RetrofitService.getServico().consulta(latitudes,longitudes);
+
+        call.enqueue(new Callback<Exemple>() {
             @Override
-            public void onResponse(retrofit2.Call<Currently> call, Response<Currently> response) {
+            public void onResponse(retrofit2.Call<Exemple> call, Response<Exemple> response) {
                 //pega o conteúdo do corpo do do resultado - incluindo todos os dados da busca
-                Currently dadosClima = response.body();
-
+                Exemple dadosClima = response.body();
 
                 //seleciona a parte que interessa e passa a busca o valor para a imagem ser alterada
-                ImprimeImagem(dadosClima.getIcon());
+                ImprimeImagem(dadosClima.getCurrently().getIcon());
             }
+
             @Override
-            public void onFailure(retrofit2.Call<Currently> call, Throwable t) {
+            public void onFailure(retrofit2.Call<Exemple> call, Throwable t) {
                 Log.i("debug", t.getMessage());
             }
         });
--- app/src/main/java/com/example/jorda/provasub2/Service/InterfaceDeServicos.java
+++ app/src/main/java/com/example/jorda/provasub2/Service/InterfaceDeServicos.java
@@ -1,9 +1,14 @@
 package com.example.jorda.provasub2.Service;
 
 import com.example.jorda.provasub2.Model.Currently;
+import com.example.jorda.provasub2.Model.Exemple;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface InterfaceDeServicos {
-    @GET ("forecast/8eeafa93fa171bb970bfac9b03caa3a3/{latitude},{longitude}?exclude=minutely,hourly,daily,flags,alerts")
-    retrofit2.Call<Currently> consulta();
+    @GET("forecast/8eeafa93fa171bb970bfac9b03caa3a3/{latitude},{longitude}?exclud e=minutely,hourly,daily,flags,alerts")
+            Call<Exemple> consulta(@Path("latitude") double latitude,
+                                   @Path("longitude") double longitude);
 }
