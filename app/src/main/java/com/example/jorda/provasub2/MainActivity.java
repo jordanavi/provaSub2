782d6d882bea976292f53bf4eb618aa53d933ee1
jordanavi <jordanavilela01@gmail.com>
2018-12-01 18:32:26 -0200

    Final funcionando

--- app/src/main/java/com/example/jorda/provasub2/MainActivity.java
+++ app/src/main/java/com/example/jorda/provasub2/MainActivity.java
@@ -18,72 +18,61 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt;
+    EditText lat;
+    EditText longi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt = (Button) findViewById(R.id.consuta_clima);
+        lat = (EditText) findViewById(R.id.latitude);
+        longi = (EditText) findViewById(R.id.longitude);
+    }
 
-        Button bt = (Button) findViewById(R.id.consuta_clima);
-        EditText lat = (EditText)findViewById(R.id.latitude);
-        EditText longi = (EditText)findViewById(R.id.longitude);
+    public void consulta(View v) {
         final double latitudes = Double.parseDouble(lat.getText().toString());
         final double longitudes = Double.parseDouble(longi.getText().toString());
-
-        bt.setOnClickListener(new View.OnClickListener() {
+        Call<Exemple> call = RetrofitService.getServico().consulta(latitudes, longitudes);
+        call.enqueue(new Callback<Exemple>() {
             @Override
-            public void onClick(View v) {
+            public void onResponse(Call<Exemple> call, Response<Exemple> response) {
+                //pega o conteúdo do corpo do do resultado - incluindo todos os dados da busca
+                Exemple dadosClima = response.body();
 
-                Call<Exemple> call = RetrofitService.getServico().consulta(latitudes,longitudes);
+                ImageView imagem = (ImageView) findViewById(R.id.imagemClima);
 
-                call.enqueue(new Callback<Exemple>() {
-                    @Override
-                    public void onResponse(retrofit2.Call<Exemple> call, Response<Exemple> response) {
-                        //pega o conteúdo do corpo do do resultado - incluindo todos os dados da busca
-                        Exemple dadosClima = response.body();
+                String i = dadosClima.getCurrently().getIcon();
+                Log.i("teste", "icon: " + i);
 
-                        ImageView imagem = (ImageView) findViewById(R.id.imagemClima);
+                //Relacionar com os dados recebidos pelo servico
+                if (i.equals("rain")) {
+                    imagem.setImageResource(R.drawable.rain);
+                } else if (i.equals("clear-day")) {
+                    imagem.setImageResource(R.drawable.clearday);
+                } else if (i.equals("clear-night")) {
+                    imagem.setImageResource(R.drawable.clearnight);
+                } else if (i.equals("snow")) {
+                    imagem.setImageResource(R.drawable.snow);
+                } else if (i.equals("sleet")) {
+                    imagem.setImageResource(R.drawable.sleet);
+                } else if (i.equals("wind")) {
+                    imagem.setImageResource(R.drawable.wind);
+                } else if (i.equals("fog")) {
+                    imagem.setImageResource(R.drawable.fog);
+                } else if (i.equals("cloudy")) {
+                    imagem.setImageResource(R.drawable.cloud);
+                } else if (i.equals("partly-cloudy-night")) {
+                    imagem.setImageResource(R.drawable.partlycloudynight);
+                } else if (i.equals("partly-cloudy-day")) {
+                    imagem.setImageResource(R.drawable.partlycloudyday);
+                }
+            }
 
-                        String i = dadosClima.getCurrently().getIcon();
-
-                        //Relacionar com os dados recebidos pelo servico
-                        if (i.equals("rain")){
-                            imagem.setImageResource(R.drawable.rain);
-                        }
-                        else if (i.equals("clear-day")){
-                            imagem.setImageResource(R.drawable.clearday);
-                        }
-                        else if (i.equals("clear-night")){
-                            imagem.setImageResource(R.drawable.clearnight);
-                        }
-                        else if (i.equals("snow")){
-                            imagem.setImageResource(R.drawable.snow);
-                        }
-                        else if (i.equals("sleet")){
-                            imagem.setImageResource(R.drawable.sleet);
-                        }
-                        else if (i.equals("wind")){
-                            imagem.setImageResource(R.drawable.wind);
-                        }
-                        else if (i.equals("fog")){
-                            imagem.setImageResource(R.drawable.fog);
-                        }
-                        else if (i.equals("cloudy")){
-                            imagem.setImageResource(R.drawable.cloud);
-                        }
-                        else if (i.equals("partly-cloudy-night")){
-                            imagem.setImageResource(R.drawable.partlycloudynight);
-                        }
-                        else if (i.equals("partly-cloudy-day")){
-                            imagem.setImageResource(R.drawable.partlycloudyday);
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(retrofit2.Call<Exemple> call, Throwable t) {
-                        Log.i("debug", t.getMessage());
-                    }
-                });
+            @Override
+            public void onFailure(Call<Exemple> call, Throwable t) {
+                Log.i("debug", t.getMessage());
             }
         });
     }
